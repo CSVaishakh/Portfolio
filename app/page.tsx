5879bc0fa01b3f4b36854074e66ef84a19451f98
Your Name <vaishakhsubramonian@gmail.com>
2025-06-25 22:55:26 +0530

    added the about section to teh strucutre

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Banner from "@/components/banner";
+import About from "@/components/about";
 
 export default function Home() {
   return (
     <section>
-      <section id="banner">
-        <Banner/>
-      </section>
+      <section id="banner"><Banner/></section>
+      <section id="about"><About/></section>
     </section>
   );
 }
