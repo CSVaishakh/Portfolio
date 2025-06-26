05467e3f22fc688e5b47b98ba68306c9f3ad701d
Your Name <vaishakhsubramonian@gmail.com>
2025-06-26 11:40:54 +0530

    added the skills section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
 import Banner from "@/components/banner";
 import About from "@/components/about";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <section>
       <section id="banner"><Banner/></section>
       <section id="about"><About/></section>
+      <section id="skills"><Skills/></section>
     </section>
   );
 }
