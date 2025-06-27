f491f983b0450097b02a95a851c7a19bacf18107
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-06-27 14:16:57 +0530

    added the projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import ProjectCard from "@/components/projectCard";
+import { Project } from "@/types/project";
+import projectData from './projectData.json'
+
+const Projects: React.FC = () => {
+    const data: Project[] = Object.values(projectData.projects)
+
+    return(
+        <section>
+            <div>
+                {data.map((project, index) => (<ProjectCard key={index} project={project}/>))}
+            </div>
+        </section>
+    )
+}
+
+export default Projects;
