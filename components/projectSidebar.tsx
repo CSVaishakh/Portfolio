3c62debfe88323cf2c688658d74f0c64df14f0a6
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-06-27 23:56:56 +0530

    re positioned the poject sidebar in the mobile layout

--- components/projectSidebar.tsx
+++ components/projectSidebar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Sidebar: React.FC = () => {
     return(
-        <section className="fixed top-1/2 left-6 -translate-y-1/2 z-50 flex flex-col gap-6 bg-gradient-to-b from-blue-100 via-white to-blue-100 backdrop-blur-md rounded-2xl shadow-xl px-4 py-8 animate-float-bar">
+        <section className="fixed bottom-20 left-1/2 -translate-x-1/2 sm:top-1/2 sm:left-6 sm:-translate-y-1/2 sm:translate-x-0 sm:bottom-auto z-50 flex flex-row sm:flex-col gap-4 sm:gap-6 bg-gradient-to-b from-blue-100 via-white to-blue-100 backdrop-blur-md rounded-2xl shadow-xl px-4 py-3 sm:py-8 animate-float-bar">
             <Link href='/'><Image src={'/home.svg'} alt={"Home"} height={24} width={24} className="opacity-80 hover:opacity-100 transition" /></Link>
             <Link href={'https://www.linkedin.com/in/csvaishakh/'}><Image src={'/linkedin.svg'} alt={'LinkedIn'} width={24} height={24} className="opacity-80 hover:opacity-100 transition" /></Link>
             <Link href={'https://github.com/CSVaishakh'}><Image src={'/github.svg'} alt={'GitHub'} width={24} height={24} className="opacity-80 hover:opacity-100 transition" /></Link>
