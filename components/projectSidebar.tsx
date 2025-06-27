258ca18f6e570fc48abd64e210f5313e5368cd44
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-06-27 16:33:14 +0530

    added the sidebar

---
+++ components/projectSidebar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Sidebar: React.FC = () => {
+    return(
+        <section className="fixed top-1/2 left-6 -translate-y-1/2 z-50 flex flex-col gap-6 bg-gradient-to-b from-blue-100 via-white to-blue-100 backdrop-blur-md rounded-2xl shadow-xl px-4 py-8 animate-float-bar">
+            <Link href='/'><Image src={'/home.svg'} alt={"Home"} height={24} width={24} className="opacity-80 hover:opacity-100 transition" /></Link>
+            <Link href={'https://www.linkedin.com/in/csvaishakh/'}><Image src={'/linkedin.svg'} alt={'LinkedIn'} width={24} height={24} className="opacity-80 hover:opacity-100 transition" /></Link>
+            <Link href={'https://github.com/CSVaishakh'}><Image src={'/github.svg'} alt={'GitHub'} width={24} height={24} className="opacity-80 hover:opacity-100 transition" /></Link>
+            <Link href={'https://x.com/vaishakh_CS'}><Image src={'/x.svg'} alt={'X'} width={24} height={24} className="opacity-80 hover:opacity-100 transition" /></Link>
+        </section>
+    )
+}
+
+export default Sidebar;
