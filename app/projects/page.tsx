e9462c10e76188b00895b7620e6e34796f8fd06a
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-06-27 14:24:31 +0530

    created the projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ProjectCard from "@/components/projectCard";
 import { Project } from "@/types/project";
 import projectData from './projectData.json'
