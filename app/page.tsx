ff53a9628ff85b9fdf298f69585af5fed5dcbf5d
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-06-27 11:34:50 +0530

    added the contact component to the page strucutre

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/banner";
 import About from "@/components/about";
 import Skills from "@/components/skills";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <section id="banner"><Banner/></section>
       <section id="about"><About/></section>
       <section id="skills"><Skills/></section>
+      <section id="contact"><Contact/></section>
     </section>
   );
 }
